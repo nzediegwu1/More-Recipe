3311a77d54edee85e2ef884c05beae53c2c3382f
nzediegwu1 <nzediegwu1@outlook.com>
2017-09-29 20:04:32 +0100

    ch(basic-tests-and-fixes):
    *corrected some es5 synthax to es6
    *update package.json for coverage integration
    *write basic experimental tests

---
+++ server/app.js
@@ -0,0 +1,16 @@
+﻿import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import recipes from './routes/recipes.js';
+// import users from './routes/users';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/recipes', recipes);
+
+export default app;
