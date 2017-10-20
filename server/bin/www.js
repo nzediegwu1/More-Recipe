20b8083d085b3a5b1661991e73eabc2cdeef0226
nzediegwu1 <nzediegwu1@outlook.com>
2017-10-20 20:28:45 +0100

    ch(www.js):
    *modified the port due to heroku h10 error

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,9 +1,10 @@
 ﻿import app from '../app';
 import models from '../models';
-app.set('port', process.env.PORT || 4000);
+
+const port = process.env.PORT || 3000;
 
 models.sequelize.sync().then(() => {
-    const server = app.listen(app.get('port'), () => {
-        console.log(`Express Server Listening on Port  ${server.address().port}`);
+    app.listen(port, () => {
+        console.log(`Express Server Listening on Port  ${this.address().port}`);
     });
 });
